daad09b8efcd27a201057e31c2a4a6a1a82f58a9
Neto 2 <idervaljose00@gmail.com>
2025-05-08 12:23:29 -0300

    feat add new file ex

---
+++ Exercicios/ex_11.c
@@ -0,0 +1,22 @@
+/* função */
+
+#include <stdio.h>
+
+int function1 (parametro1){
+    if (parametro1 % 2 == 0){
+        return printf("%d é um numero par!",parametro1);
+    }else{
+        return printf("%d é um numero impar!,parametro1");
+    }
+}
+
+int main(){
+    
+    int parametro1;
+
+    printf("\nDigite o numero: ");
+    scanf("%d",&parametro1);
+
+    function1(parametro1);
+
+}
