38616d19e8e595410372900f14c9f8a49264d048
Neto 2 <idervaljose00@gmail.com>
2025-05-15 11:39:44 -0300

    feat add new file ex

---
+++ Exercicios/ex_14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void desenhar(repeticao){
+    int acc = 0;
+    while (acc < repeticao)
+        
+        printf("")
+
+    acc++
+}
+
+int main () {
+    int repeticao;
+    printf("numero: ");
+    scanf("%d",&repeticao);
+
+    desenhar(repeticao);
+
+
+
+
+
+
+}
---
+++ Exercicios/ex_15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+
+    char  asterisco = "*";
+
+    printf("%c",asterisco*4);
+
+
+
+}
